d90243bdf990fbcdfa6ef5d1ced0cdcf436bc705
Jontheittractorguy <112268544+Jontheittractorguy@users.noreply.github.com>
2023-02-14 13:50:48 -0700

    made calculator
    made css, js, and html for calculator
    added all functions but working on clear text from equation

---
+++ calc.js
@@ -0,0 +1,156 @@
+const typed = document.getElementById('typedDisplay');
+const operation = document.getElementById('operationDisplay');
+const percent = document.getElementById('percent');
+const clearEvery = document.getElementById('clearEvery');
+const clear = document.getElementById('clear');
+const divide = document.getElementById('divide');
+const one = document.getElementById('1');
+const two = document.getElementById('2');
+const three = document.getElementById('3');
+const four = document.getElementById('4');
+const five = document.getElementById('5');
+const six = document.getElementById('6');
+const seven = document.getElementById('7');
+const eight = document.getElementById('8');
+const nine = document.getElementById('9');
+const zero = document.getElementById('0');
+const multiply = document.getElementById('multiply');
+const subtract = document.getElementById('subtract');
+const add = document.getElementById('add');
+const equal = document.getElementById('equal');
+const negative = document.getElementById('negative');
+const period = document.getElementById('period');
+let operand = [];
+let number;
+let equalPressed = false;
+let lastPressed;
+
+one.onclick = (e) => text(e.target.value);
+two.onclick = (e) => text(e.target.value);
+three.onclick = (e) => text(e.target.value);
+four.onclick = (e) => text(e.target.value);
+five.onclick = (e) => text(e.target.value);
+six.onclick = (e) => text(e.target.value);
+seven.onclick = (e) => text(e.target.value);
+eight.onclick = (e) => text(e.target.value);
+nine.onclick = (e) => text(e.target.value);
+zero.onclick = (e) => text(e.target.value);
+add.onclick = () => display("adds");
+multiply.onclick = () => display("multiplies");
+divide.onclick = () => display("divides");
+subtract.onclick = () => display("subtracts");
+equal.onclick = () => operates(operand);
+clear.onclick = () => clearText();
+clearEvery.onclick = () => cleared();
+
+
+// functions
+function equals(final){   
+    operation.textContent += "=" ;
+    typed.textContent = final;
+}
+
+function text(value){
+    if (equalPressed == true){
+        cleared();
+        operation.textContent += value;
+        typed.textContent = value;
+        lastPressed = value;
+        number += value;
+    } else {
+        operation.textContent += value;
+        typed.textContent = value;
+        lastPressed = value;
+        number += value;
+    }
+}
+
+function display(arg){
+    if (arg == "adds" || "divides" || "subracts" || "multiplies"){
+        operand.unshift(arg)
+    }
+    if (arg == "adds"){
+        operation.textContent += "+";
+        typed.textContent = "+"
+        lastPressed = "+";
+    } else if (arg == "divides") {
+        operation.textContent += divide.textContent;;
+        typed.textContent = divide.textContent;
+        lastPressed = divide.textContent;
+    } else if (arg == "multiplies"){
+        operation.textContent = multiply.textContent;
+        typed.textContent = multiply.textContent;
+        lastPressed = multiply.textContent;
+    } else if (arg == "subtracts"){
+        operation.textContent = "-";
+        typed.textContent = "-";
+        lastPressed = "-";
+    }
+    operand.push(Number(number));
+    number = "";
+}
+
+function clearText(){
+    if (equalPressed == true){
+        cleared();
+    } else {
+        operand.pop();
+        let text = operation.textContent;
+        console.log(typeof lastPressed);
+        let reg = new RegExp(lastPressed,"g")
+        text.replace(reg,"");
+        console.log(text);
+        operation.textContent = text;
+    }
+}
+
+function adds(arguments){
+    const args = arguments;
+    return args.reduce((a, b) => (a + b));
+}
+
+function subtracts(arguments){
+    const args = arguments;
+    return args.reduce((a, b) => (a - b));
+}
+
+function divides(arguments){
+    const args = arguments;
+    return args.reduce((a, b) => (a/b));
+}
+
+function multiplies(arguments){
+    const args = arguments;
+    return args.reduce((a, b) => (a * b));
+}
+
+function percentage(){
+
+}
+
+function operates(array){
+    operand.push(Number(number));
+    let equation;
+    if (array[0] == "adds"){
+        equation = adds(array.slice(1));
+    } else if (array[0] == "subtracts"){
+        equation = subtracts(array.slice(1));
+    } else if (array[0] == "multiplies"){
+        equation = multiplies(array.slice(1));
+    } else if (array[0] == "divides"){
+        equation = divides(array.slice(1));
+    }
+    operand = []
+    equalPressed = true
+    equals(equation);
+}
+
+function cleared(){
+    equalPressed = false;
+    number = "";
+    operand = [];
+    typed.textContent = "";
+    operation.textContent = "";
+}
+
+window.onload = cleared();

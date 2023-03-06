76aad657704dd03679ed0a08dabf13c6a421bcff
Jontheittractorguy <112268544+Jontheittractorguy@users.noreply.github.com>
2023-03-06 15:11:01 -0700

    wiped out javascript and started over
    found better way to do javascript so you could do multiple
    operations in the same equation.
    redid the clear and clear everything buttons and made it so it stores
    last total and you can use as part of new equation.

--- calc.js
+++ calc.js
@@ -1,231 +1,122 @@
-const typed = document.getElementById('typedDisplay');
-const operation = document.getElementById('operationDisplay');
-const percent = document.getElementById('percent');
-const clearEvery = document.getElementById('clearEvery');
-const clear = document.getElementById('clear');
-const divide = document.getElementById('divide');
-const one = document.getElementById('1');
-const two = document.getElementById('2');
-const three = document.getElementById('3');
-const four = document.getElementById('4');
-const five = document.getElementById('5');
-const six = document.getElementById('6');
-const seven = document.getElementById('7');
-const eight = document.getElementById('8');
-const nine = document.getElementById('9');
-const zero = document.getElementById('0');
-const multiply = document.getElementById('multiply');
-const subtract = document.getElementById('subtract');
-const add = document.getElementById('add');
-const equal = document.getElementById('equal');
-const negative = document.getElementById('negative');
-const period = document.getElementById('period');
 let operand = [];
-let clearing = false;
-let number;
-let sign = false;
-let equalPressed = false;
-let lastPressed;
+let operates = "";
+let signs = false;
+let equalSign = false;
 
-one.onclick = (e) => text(e.target.value);
-two.onclick = (e) => text(e.target.value);
-three.onclick = (e) => text(e.target.value);
-four.onclick = (e) => text(e.target.value);
-five.onclick = (e) => text(e.target.value);
-six.onclick = (e) => text(e.target.value);
-seven.onclick = (e) => text(e.target.value);
-eight.onclick = (e) => text(e.target.value);
-nine.onclick = (e) => text(e.target.value);
-zero.onclick = (e) => text(e.target.value);
-add.onclick = () => display("adds");
-multiply.onclick = () => display("multiplies");
-divide.onclick = () => display("divides");
-subtract.onclick = () => display("subtracts");
-equal.onclick = () => operates(operand);
-clear.onclick = () => clearText();
-clearEvery.onclick = () => cleared();
-percent.onclick = () => percentage();
+// listen for key press
+const calculator = document.querySelector("#calcBody");
+const keys = calculator.querySelector("#buttons");
+const display = document.querySelector("#typedDisplay");
+const operation = document.querySelector("#operationDisplay");
+const total = document.querySelector(".totally");
 
+keys.addEventListener('click',e => {
+    if (e.target.matches('button')){
+        const key = e.target;
+        const action = key.dataset.action;
+        let content = key.textContent;
+        const displayNumb = display.textContent;
+        // display
+        if (typeof action != "string"){
 
-// functions
-// function for display after equal is pressed.
-function equals(final){   
-    operation.textContent += "=" ;
-    typed.textContent = final;
-}
-
-// function for number buttons
-function text(value){
-    if (equalPressed == true){
-        cleared();
-        operation.textContent += value;
-        typed.textContent = value;
-        lastPressed = value;
-        number = value;
-        operand.push(Number(number));
-    } else if (sign == true){
-        operation.textContent += value;
-        typed.textContent = value;
-        lastPressed = value;
-        number = value;
-        operand.push(Number(number));
-    } else if(clearing == true){
-        operation.textContent += value;
-        typed.textContent += value;
-        lastPressed = value;
-        number = value;
-        operand.push(Number(number));
-    } else {
-        operation.textContent += value;
-        typed.textContent = value;
-        lastPressed = value;
-        number += value;
-        operand.splice(-1);
-        operand.push(Number(number));
-    }
-    equalPressed = false;
-    clearing = false;
-    sign = false;
-}
-
-// function to show equation symbols in display
-function display(arg){
-    if (arg == "adds" || "divides" || "subracts" || "multiplies"){
-        operand.unshift(arg)
-        sign = true;
-    }
-    if (arg == "adds"){
-        operation.textContent += "+";
-        typed.textContent = "+"
-        lastPressed = "+";
-    } else if (arg == "divides") {
-        operation.textContent += divide.textContent;;
-        typed.textContent = divide.textContent;
-        lastPressed = divide.textContent;
-    } else if (arg == "multiplies"){
-        operation.textContent += multiply.textContent;
-        typed.textContent = multiply.textContent;
-        lastPressed = multiply.textContent;
-    } else if (arg == "subtracts"){
-        operation.textContent += "-";
-        typed.textContent = "-";
-        lastPressed = "-";
-    }
-    number = "";
-}
-
-// function to  clear display
-function clearText(){
-    if (equalPressed == true){
-        cleared();
-    } else if (check(operand)){
-        clearing = true;
-        if (sign == true){
-            operand.shift();
-            sign = false;
-            let text = operation.textContent;
-            let newest = text.replace(lastPressed,"");
-            operation.textContent = newest;
-            typed.textContent = "";
-        } else {
-            operand.pop();
-            let text = operation.textContent;
-            remove();
-            typed.textContent = "";
-            number = "";
+            if (display.textContent === "0" || signs == true){
+                display.textContent = content;
+                operates += content;
+            } else if (equalSign == true){
+                clearAll();
+                display.textContent = content;
+                operates = content;
+            } else {
+                display.textContent = displayNumb + content;
+                operates += content;
+            }
+            signs = false;
+            equalSign = false;
+        } else if (action == "equal"){
+            let totals = operate(operand);
+            display.textContent = totals;
+            total.textContent = totals;
+            equalSign = true;
+            content = "";
+        } else if (action == "plusMinus") {
+            plusMinus(displayNumb);
+            content = "";
         }
-    } else {
-        clearing = true;
-        if (sign == true){
-            operand.shift();
-            sign = false;
+        else if (action == "CE"){
+            clearAll();
+            content = "";
+        } else if (action == "clear"){
+            clear();
+            content = "";
         } else {
-            operand.pop();
-            number = "";
+            if (equalSign == true){
+                clearAll();
+                operand.push(total.textContent);
+                operates += total.textContent;                
+            }
+            display.textContent = content;
+            operates += content;
+            signs = true;
         }
-        let text = operation.textContent;
-        let newest = text.replace(lastPressed,"");
-        operation.textContent = newest;
-        typed.textContent = "";
+        if (content != ""){
+            operand.push(content)
+        };
+        operation.textContent = operates;
     }
-}
+});
 
-function adds(arguments){
-    const args = arguments;
-    return args.reduce((a, b) => (a + b));
-}
-
-function subtracts(arguments){
-    const args = arguments;
-    return args.reduce((a, b) => (a - b));
-}
-
-function divides(arguments){
-    const args = arguments;
-    return args.reduce((a, b) => (a/b));
-}
-
-function multiplies(arguments){
-    const args = arguments;
-    return args.reduce((a, b) => (a * b));
-}
-
-function percentage(){
-    const word = operation.textContent.lastIndexOf(number);
-    const firstPart = operation.textContent.slice(0,word);
-    operation.textContent = firstPart;
-    operand.pop();
-    number = number/100;
-    operation.textContent += ((number * 100) + "%");
-    operand.push(number);
-    number = "";
-}
-
-// run equation
-function operates(array){
-    let equation;
-    if (array[0] == "adds"){
-        equation = adds(array.slice(1));
-    } else if (array[0] == "subtracts"){
-        equation = subtracts(array.slice(1));
-    } else if (array[0] == "multiplies"){
-        equation = multiplies(array.slice(1));
-    } else if (array[0] == "divides"){
-        equation = divides(array.slice(1));
+// function for operation
+function operate(array){
+    equalSign = true;
+    if (isNaN(array.slice(-1)) || isNaN(array.slice(0,1))){
+        return "OOPS";
     }
-    console.log(array.slice[1]);
-    operand = []
-    equalPressed = true
-    equals(equation);
+    const arry = array.join("");
+    const times = arry.toString().replace("\u00F7","/");
+    const multy = times.toString().replace("\u00D7","*")
+    const equals = multy;
+    return eval(equals);
 }
 
-// clears calculator
-function cleared(){
-    equalPressed = true;
-    sign = false;
-    number = "";
+// clear all
+function clearAll(){
+    signs = false;
     operand = [];
-    typed.textContent = "";
-    operation.textContent = "";
+    operates = "";
+    display.textContent = "0";
+    operation.textContent = "0";
 }
 
-// function to check if operand has more than one iteration
-function check(arr){
-    const map = {};
-    for (const item of arr){
-        if (map[item]){
-            return true;
-        }
-        map[item] = true;
+// clear
+function clear(){
+    operand.pop();
+    let removed = operates.slice(0,operates.length - 1);
+    operates = removed;
+    display.textContent = "";
+}
+
+// plus and minus (give negative sign)
+function plusMinus(x){
+    console.log(x);
+    let numb;
+    for (let i = 0;i<x.length;i++){
+        operand.pop();
     }
-    return false;
+    if (x > 0){
+        operand.push(x);
+        operand.push("*");
+        operand.push("-1");
+        numb = x * -1;
+    } else if (x < 0){
+        numb = Math.abs(x);
+        operand.push(numb);
+    }
+    let removed = operates.slice(0,operates.length - x.length);
+    operates = removed + numb;
+    display.textContent = numb;
 }
 
-// function to remove equation display when clear is used
-function remove(){
-    const word = operation.textContent.lastIndexOf(lastPressed);
-    const firstPart = operation.textContent.slice(0,word);
-    operation.textContent = firstPart;
-}
+// percentage button
+function percentage(){
 
-window.onload = ()=> {cleared(); typed.textContent = "Hello"};
+}
